4a28ec4a1c07ab709317c13d85e100868cc992b2
temppone <gtvtempone@gmail.com>
2021-11-13 10:00:44 -0300

    feat: money formatter

---
+++ src/core/toMoney.ts
@@ -0,0 +1,8 @@
+export const toMoney = (value: number, currency: string, locale: string): string => {
+    const formatter = new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency,
+        minimumFractionDigits: 2,
+    });
+    return formatter.format(value);
+}
