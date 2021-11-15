f306112ce6a226cd23b6aeac5dc147f67f19dce2
temppone <gtvtempone@gmail.com>
2021-11-14 21:07:50 -0300

    fix: Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,12 +5,30 @@ import { ButtonContainer, ButtonContent } from "./style";
 interface Props {
   name: string;
   svg?: ReactElement;
+  background?: string;
+  color?: string;
+  borderRadius?: string;
+  width?: string;
+  padding?: string;
+  fontSize?: string;
 }
 
-const Button = ({ name, svg }: Props): JSX.Element => {
+const Button = ({
+  name,
+  svg,
+  background,
+  color,
+  borderRadius,
+  width,
+  padding,
+  fontSize,
+}: Props): JSX.Element => {
   return (
     <ButtonContainer>
-      <ButtonContent>
+      <ButtonContent
+        type="submit"
+        style={{ background, color, borderRadius, width, padding, fontSize }}
+      >
         {svg}
         {name}
       </ButtonContent>
--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -9,6 +9,7 @@ export const ButtonContent = styled.button`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  justify-content: center;
   padding: 0.5rem;
   background-color: transparent;
   color: ${() => defaultTheme.palette.primaryBlue};
