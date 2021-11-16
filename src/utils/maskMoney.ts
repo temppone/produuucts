c5366ff27e8d0379b17c3e19a48f96d1431c1a2b
temppone <gtvtempone@gmail.com>
2021-11-16 17:42:24 -0300

    chore: rename core to utils

---
+++ src/utils/maskMoney.ts
@@ -0,0 +1,12 @@
+export const maskReais = (value: string): string => {
+  return (Number(value.replace(/\D/g, "")) / 100).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+};
+
+export const unMaskReais = (value: number | string): number | string => {
+  return typeof value === "number"
+    ? value
+    : Number(value.replace(/\D/g, "")) / 100;
+};
