87df9386d697f56100a47193d41187d428c2d1eb
temppone <gtvtempone@gmail.com>
2021-11-16 06:36:37 -0300

    fix: dropdown component

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -9,12 +9,17 @@ import {
 
 interface IDropdown {
   children?: ReactNode;
-  dropdownItems?: ReactNode[];
+  dropdownItems?: {
+    label: string;
+    onClick?: () => void;
+  }[];
+  onClick?: React.MouseEventHandler<HTMLLIElement> | undefined;
 }
 
 const Dropdown = ({ children, dropdownItems }: IDropdown): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
   const toggling = () => setIsOpen(!isOpen);
+
   return (
     <DropdownContainer>
       <DropdownHeader onClick={toggling}>{children}</DropdownHeader>
@@ -22,7 +27,9 @@ const Dropdown = ({ children, dropdownItems }: IDropdown): JSX.Element => {
         {isOpen && (
           <DropdownList>
             {dropdownItems?.map((item, index) => (
-              <DropdownItem key={index}>{item}</DropdownItem>
+              <DropdownItem key={index} onClick={item.onClick}>
+                {item.label}
+              </DropdownItem>
             ))}
           </DropdownList>
         )}
