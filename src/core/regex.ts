37c63e3ae3b0c2a50ff70e7b0f9fcd3fa7ebd7ce
temppone <gtvtempone@gmail.com>
2021-11-16 17:41:15 -0300

    fix: delete some files

--- src/core/regex.ts
+++
@@ -1,1 +0,0 @@
-export const numbersAndLetters = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i;
--- src/core/toMoney.ts
+++
@@ -1,9 +0,0 @@
-export const toMoney = (value: number, currency: string, locale: string): string => {
-
-    const formatter = new Intl.NumberFormat(locale, {
-        style: 'currency',
-        currency,
-        minimumFractionDigits: 2,
-    });
-    return formatter.format(value);
-}
