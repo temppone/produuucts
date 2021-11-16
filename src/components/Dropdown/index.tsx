c52068a339766f8752637c4ef96ce1eb36125488
temppone <gtvtempone@gmail.com>
2021-11-16 17:44:52 -0300

    fix: dropdown component

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -3,6 +3,8 @@ import {
   DropdownContainer,
   DropdownHeader,
   DropdownItem,
+  DropdownItemLabel,
+  DropdownItemSvg,
   DropdownList,
   DropdownListContainer,
 } from "./styles";
@@ -10,10 +12,13 @@ import {
 interface IDropdown {
   children?: ReactNode;
   dropdownItems?: {
+    svg?: ReactNode;
     label: string;
     onClick?: () => void;
+    labelColor?: string;
   }[];
   onClick?: React.MouseEventHandler<HTMLLIElement> | undefined;
+  isOpen?: boolean;
 }
 
 const Dropdown = ({ children, dropdownItems }: IDropdown): JSX.Element => {
@@ -27,8 +32,17 @@ const Dropdown = ({ children, dropdownItems }: IDropdown): JSX.Element => {
         {isOpen && (
           <DropdownList>
             {dropdownItems?.map((item, index) => (
-              <DropdownItem key={index} onClick={item.onClick}>
-                {item.label}
+              <DropdownItem
+                key={index}
+                onClick={() => {
+                  item.onClick?.();
+                  toggling();
+                }}
+              >
+                <DropdownItemSvg>{item.svg}</DropdownItemSvg>
+                <DropdownItemLabel style={{ color: item.labelColor }}>
+                  {item.label}
+                </DropdownItemLabel>
               </DropdownItem>
             ))}
           </DropdownList>
--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -10,6 +10,7 @@ export const DropdownHeader = styled("div")`
   margin-bottom: 0.1em;
   font-weight: 500;
   font-size: 1.3rem;
+  cursor: pointer;
 `;
 
 export const DropdownListContainer = styled("div")`
@@ -19,7 +20,7 @@ export const DropdownListContainer = styled("div")`
 `;
 
 export const DropdownList = styled("ul")`
-  width: 40%;
+  width: 30%;
   margin: 0;
   background-color: ${defaultTheme.palette.primaryLight};
   border: 1px solid ${defaultTheme.palette.primaryGrey};
@@ -28,13 +29,50 @@ export const DropdownList = styled("ul")`
   font-size: 1rem;
   position: absolute;
   padding: 0.5rem;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    max-width: 15%;
+  }
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    max-width: 13%;
+  }
+  @media (min-width: ${({ theme }) => theme.breakpoints.xs}) {
+    width: 12%;
+  }
+
   &:first-child {
     padding-top: 0.8em;
   }
 `;
 
 export const DropdownItem = styled("li")`
+  display: flex;
+  align-items: center;
+  align-content: center;
   list-style: none;
   margin-bottom: 0.8em;
-  padding: 0.1rem;
+  padding: 0.5rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${defaultTheme.palette.primaryGrey};
+  }
+`;
+
+export const DropdownItemLabel = styled.div`
+  margin-right: 0.5rem;
+`;
+
+export const DropdownItemSvg = styled.div`
+  margin-right: 0.5rem;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    width: 50px;
+  }
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    width: 60px;
+  }
+  @media (min-width: ${({ theme }) => theme.breakpoints.xs}) {
+    width: 70px;
+  }
 `;
