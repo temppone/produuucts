14f762e5ff051863e7fd110a8ae698392089ebf3
temppone <gtvtempone@gmail.com>
2021-11-16 06:38:11 -0300

    fix: home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,59 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IProduct } from "../../@types";
 import { ReactComponent as Plus } from "../../assets/plus.svg";
 import Button from "../../components/Button";
 import { CardContainer } from "../../components/Card/style.";
 import HeaderCard from "../../components/HeaderCard";
+import HeaderPage from "../../components/HeaderPage";
 import ProductCard from "../../components/ProductCard";
 import { toMoney } from "../../core/toMoney";
-import { HomeContainer } from "./styles";
+import { deleteProduct, getProducts } from "../../services/productService";
+import { EmptyProductsContainer, HomeContainer } from "./styles";
 
 const Home = (): JSX.Element => {
-  const items = [
-    {
-      id: "A12J2",
-      category: "Eletrônico",
-      name: "Notebook G40",
-      providerName: "Lenovo",
-      price: 2010,
-    },
+  const [products, setProducts] = React.useState<IProduct[]>([]);
+  const navigate = useNavigate();
 
-    {
-      id: "2DK20",
-      category: "Eletrônico",
-      name: "Celular AJ5",
-      providerName: "Samsung",
-      price: 1100,
-    },
-
-    {
-      id: "1229J",
-      category: "Eletrônico",
-      name: "Tablet JJ22",
-      providerName: "Samsung",
-      price: 22300,
-    },
-    {
-      id: "12J92",
-      category: "Eletrônico",
-      name: "iPad Plus 100",
-      providerName: "Samsung",
-      price: 22300,
-    },
-    {
-      id: "12J92",
-      category: "Eletrônico",
-      name: "Fita isolante",
-      providerName: "Samsung",
-      price: 22300,
-    },
-  ];
+  useEffect(() => {
+    getProducts().then(setProducts);
+  }, [products]);
 
   return (
     <HomeContainer>
+      <HeaderPage headerTitle="Olá, você está na Produuucts" />
+
       <CardContainer>
         <HeaderCard title="Produtos">
-          <Link to={"registration"}>
+          <Link to={"product/create"}>
             <Button
               svg={<Plus style={{ marginRight: "0.4rem", width: "0.9rem" }} />}
               name="Adicionar produto"
@@ -61,17 +33,28 @@ const Home = (): JSX.Element => {
           </Link>
         </HeaderCard>
 
-        {items.map((item) => (
+        {products.map((item) => (
           <ProductCard
             key={item.id}
             id={item.id}
             category={item.category}
             name={item.name}
             providerName={item.providerName}
-            price={toMoney(item.price, "BRL", "pt-BR")}
+            price={toMoney(Number(item.price), "BRL", "pt-BR")}
+            onEdit={() => navigate(`/product/edit/${item.id}`)}
+            onDelete={async () => {
+              await deleteProduct(item.id);
+              await getProducts();
+            }}
           />
         ))}
       </CardContainer>
+
+      {products.length === 0 && (
+        <EmptyProductsContainer>
+          Não há nenhum produto cadastrado ainda.
+        </EmptyProductsContainer>
+      )}
     </HomeContainer>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,14 @@ import styled from "styled-components";
 import { defaultTheme } from "../../styles/theme";
 
 export const HomeContainer = styled.div`
-  background-color: ${defaultTheme.palette.secundaryLight};
+  background-color: ${defaultTheme.palette.primaryGrey};
   border-radius: 0.9rem;
+`;
+
+export const EmptyProductsContainer = styled.div`
+  background-color: ${defaultTheme.palette.primaryGrey};
+
+  padding: 2rem;
+  font-size: 1.4rem;
+  height: 100vw;
 `;
